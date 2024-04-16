f21420b81c5a363b2a64d8e6c3c49864bff54709
kaylee125 <leesh970930@naver.com>
2024-04-16 22:34:27 +0900

    solved python leetcode

--- py_algorithm_interview/1_two_sum.py
+++ py_algorithm_interview/1_two_sum.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
         for i,n in enumerate(nums):
-            
             sub_n=target-n
-            if sub_n in nums[i+1:]:
-                
-                return [nums.index(n),nums.index(sub_n)]
+            if sub_n in nums[i+1:] :
+                return [nums.index(n),nums[i+1:].index(sub_n)+(i+1)]
                 
 
 sol=Solution()
-nums = [3,2,4]
+nums = [3,2,3]
 target = 6
 res=sol.twoSum(nums,target)
 print(res)
